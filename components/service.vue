66fb2ef489e1353341854abda431cdec82ba8199
DuncanLin <ken21181238@gmail.com>
2025-08-05 18:41:39 +0800

    service + rwd

--- components/service.vue
+++ components/service.vue
@@ -51,6 +51,32 @@ const plans = ref([
         pic: new URL('../assets/images/service/plan-3.svg', import.meta.url).href
     },
 ])
+const consults = ref([
+    {
+        hot: false,
+        title: "職涯探索評估",
+        desc: "3 次一對一諮詢，了解可能性和適配性",
+        price: 3800,
+        link: "#",
+        pic: new URL('../assets/images/service/server-1.svg', import.meta.url).href
+    },
+    {
+        hot: true,
+        title: "職涯轉型規劃",
+        desc: "6 次深度諮詢，建立完整轉型藍圖",
+        price: 12000,
+        link: "#",
+        pic: new URL('../assets/images/service/server-2.svg', import.meta.url).href
+    },
+    {
+        hot: false,
+        title: "專業定位與品牌建立",
+        desc: "4 次專業諮詢，確立市場定位與差異化策略",
+        price: 8800,
+        link: "#",
+        pic: new URL('../assets/images/service/server-3.svg', import.meta.url).href
+    },
+])
 </script>
 <template>
     <section class="bg-[url(/assets/images/service/banner-bg.png)] bg-repeat bg-[length:200px] relative">
@@ -72,14 +98,14 @@ const plans = ref([
     <section>
         <div class="container-1296">
             <section class="py-12 p-3 md:py-20 md:px-0">
-                <div class="flex flex-col items-center gap-4 md:">
+                <div class="flex flex-col items-center gap-4">
                     <h3 class="cusText-4xl">
                         <img src="../assets/images/service/goal.svg" alt="iconImg" class="h-14 w-14 mx-auto mb-1">
                         職涯轉型全程計畫</h3>
                     <p class="cusText-b-lg text-natural-50">從企業到自由，無縫轉型之旅</p>
                 </div>
     
-                <div v-for="(plan, index) in plans" :key="index" class="rounded-card shadow-boxShadow p-6 pt-12 md:p-12 relative my-4" :class="plan.hot ? 'border-2 border-secondary-50' : 'border border-gray-200'" >
+                <div v-for="(plan, index) in plans" :key="index" class="rounded-card shadow-boxShadow p-6 md:p-12 relative my-4" :class="plan.hot ? 'border-2 border-secondary-50 pt-12' : 'border-none'" >
                     <!-- HOT 標籤 -->
                     <div v-if="plan.hot" class="absolute top-0 left-0 bg-secondary-50 text-natural-main px-4 py-2 rounded-tAD rounded-tr-none rounded-bl-none cusText-b-sm font-bold">
                         HOT
@@ -89,7 +115,7 @@ const plans = ref([
                             <div class="space-y-2">
                                 <!-- 方案標題 -->
                                 <p class="text-primary-60 cusText-b-md">{{ plan.plan }}</p>
-                                <h2 class="cusText-2xl">{{ plan.name }}</h2>
+                                <h4 class="cusText-2xl">{{ plan.name }}</h4>
                                 <p class="cusText-b-md">{{ plan.desc }}</p>
                 
                                 <!-- 特色清單 -->
@@ -138,6 +164,91 @@ const plans = ref([
         </div>
     </section>
 
+    <section class="bg-natural-95">
+        <div class="container-1296">
+            <section class="py-12 p-3 md:py-20 md:px-0">
+                <div class="flex flex-col items-center gap-4">
+                    <h3 class="cusText-4xl">
+                        <img src="../assets/images/service/conversation.svg" alt="iconImg" class="h-14 w-14 mx-auto mb-1">
+                        基礎諮詢服務</h3>
+                    <p class="cusText-b-lg text-natural-50">我們提供第一次免費諮詢，一起建立信任的橋樑</p>
+                </div>
+                
+                <div class="mt-6 md:mt-12 flex flex-col gap-4 md:gap-6 md:grid md:grid-cols-2 min-[1024px]:grid-cols-3">
+                    <div v-for="(consult, index) in consults" :key="index" class="rounded-card shadow-boxShadow relative bg-natural-light py-6 px-4 md:px-6 min-w-spac-300 md:col-span-1" :class="consult.hot ? 'border-2 border-secondary-50' : 'border-none'" >
+                        <!-- HOT 標籤 -->
+                        <div v-if="consult.hot" class="absolute top-0 left-0 bg-secondary-50 text-natural-main px-4 py-2 rounded-tAD rounded-tr-none rounded-bl-none cusText-b-sm font-bold">
+                            HOT
+                        </div>
+                        <div class="flex flex-col items-center text-center gap-4 md:gap-6">
+                            <div class="w-[120px] md:w-40">
+                                <img :src="consult.pic" alt="pic" class="w-full h-auto object-cover">
+                            </div>
+                            <div class="space-y-2">
+                                <h4 class="cusText-2xl">{{ consult.title }}</h4>
+                                <p class="cusText-b-lg text-natural-50">{{ consult.desc }}</p>
+                                <p class="cusText-3xl text-primary-60">NT$ {{ consult.price.toLocaleString() }}</p>
+        
+                            </div>
+                            
+                            <!-- 按鈕 -->
+                            <button :href="consult.link" class="btn-filled rounded-btn cusText-b-md p-3">
+                                <span class="-mb-[1px]">我有興趣
+                                </span>
+                                <span class="[&>svg]:md:w-6 [&>svg]:md:h-6">
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 0 24 24" width="20"><path d="M0 0h24v24H0z" fill="none"/><path fill="currentColor" d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"/></svg>
+                                </span>
+                            </button>
+        
+                        </div>
+    
+                    </div>
+
+                </div>
+
+            </section>
+        </div>
+    </section>
+
+
+    <section class="bg-[linear-gradient(to_bottom,_theme(colors.natural.95)_200px,_theme(colors.natural.light)_200px)]">
+        <div class="container-1296">
+            <section class="py-12 p-3 md:py-20 md:px-0">
+                <div class="flex flex-col items-center gap-4">
+                    <h3 class="cusText-4xl">
+                        <img src="../assets/images/service/love.svg" alt="iconImg" class="h-14 w-14 mx-auto mb-1">
+                        持續支持方案
+                    </h3>
+                </div>
+                
+                
+                <section class="bg-[url('/assets/images/service/subscribe-3.svg')] bg-secondary-90 bg-[length:175%] bg-no-repeat h-[265px] bg-[position:100%_0%] md:h-[326px] border-2 border-secondary-50 shadow-boxShadow rounded-card p-6 md:p-12 flex flex-col gap-12 items-center text-center max-w-[856px] mx-auto md:bg-[length:70%]">
+                    <div>
+                        <h4 class="cusText-2xl mb-2">職旅會員計畫</h4>
+                        <p class="cusText-b-md">享有長期社群支持與資源服務</p>
+    
+                    </div>
+                    <div class="space-y-2">
+                        <p class="cusText-b-md">
+                            <span class="cusText-xl text-primary-60">NT$ 9,800 </span>
+                            /年( NT$1,200 /月)
+                        </p>
+                    </div>
+                    
+                    <!-- 按鈕 -->
+                    <button href="#" class="btn-filled rounded-btn cusText-b-md p-3">
+                        <span class="-mb-[1px]">我有興趣
+                        </span>
+                        <span class="[&>svg]:md:w-6 [&>svg]:md:h-6">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 0 24 24" width="20"><path d="M0 0h24v24H0z" fill="none"/><path fill="currentColor" d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"/></svg>
+                        </span>
+                    </button>
+
+                </section>
+
+            </section>
+        </div>
+    </section>
 
 
 </template>
